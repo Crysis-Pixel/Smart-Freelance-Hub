1c0eff010db4e176a1513a6166e45186388a96e8
Sakibul Alam Jim <77940664+Crysis-Pixel@users.noreply.github.com>
2024-11-04 09:45:42 +0600

    Update updateuser.js

--- backend-smart-freelance-hub/controller/users/updateuser.js
+++ backend-smart-freelance-hub/controller/users/updateuser.js
@@ -5,7 +5,7 @@ const db_name = process.env.DATABASE_NAME;
 const collection_users = process.env.COLLECTION_USERS;
 
 exports.updateUser = async (req, res) => {
-    const {firstName, lastName, email, country, phoneNumber, accountType, fBio, cBio, skills, fRating, cRating, profilePicture, totalEarnings, lastActive, 
+    const {firstName, lastName, email, country, phoneNumber, lookingForJob, accountType, fBio, cBio, skills, fRating, cRating, profilePicture, totalEarnings, lastActive, 
         jobsCompleted
     } = req.body;
     if (!emailExist(email)) {
@@ -19,7 +19,7 @@ exports.updateUser = async (req, res) => {
         // Update the user in the database
         const result = await collection.updateOne(
             { email: email }, // Use the email from the request body
-            { $set: { firstName, lastName, email, country, phoneNumber, accountType, fBio, cBio, skills, fRating, cRating, profilePicture, totalEarnings, lastActive, 
+            { $set: { firstName, lastName, email, country, phoneNumber, lookingForJob, accountType, fBio, cBio, skills, fRating, cRating, profilePicture, totalEarnings, lastActive, 
                 jobsCompleted } }
         );
         console.log(result);
